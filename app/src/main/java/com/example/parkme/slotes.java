5c74b3bf11b1532feae6c0252bd66256da1e8bc2
saurabh kumar <saurabh160199@gmail.com>
2019-10-04 23:40:06 +0530

    detail and spots

--- app/src/main/java/com/example/parkme/slotes.java
+++ app/src/main/java/com/example/parkme/slotes.java
@@ -3,9 +3,10 @@ package com.example.parkme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class slotes extends AppCompatActivity {
-
+    TextView s1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
