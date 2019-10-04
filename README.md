732521bc35a8e9e8f16cd4ffa9bcfb14a3366869
saurabh kumar <saurabh160199@gmail.com>
2019-10-05 02:14:26 +0530

    c1

---
+++ README.md
@@ -0,0 +1,25 @@
+# Parking Management System
+
+- #### The project is all about helping all the faculty, students and others for parking their vehicles in the proper and arranged manner.
+
+- #### Through this project, we can stop or reduce conflicts related to the parking.
+
+- #### In this project we are providing the spot facility.
+
+- #### Only one spot is allotted for one vehicle, same spot is not alloted for multiple person.
+
+- #### This makes the parking more managed  rather than the original parking.
+
+- #### Through this project we can fetch the details about the vehicle and the owner.
+
+- #### A person would not be worried abou where to park his/her vehicle.
+
+- #### Time saving.
+
+- #### Reduces the work load of the user.
+- #### In case of special events, traffic congestion can be reduced.
+
+- #### This gives the information about the vehicle when it comes in and when it comes out from the parking spot.
+- #### Parking management system prevent unauthorised access to your parking spots.
+
+- #### It is versatile.
--- app/src/main/java/com/example/parkme/slotes.java
+++ app/src/main/java/com/example/parkme/slotes.java
@@ -2,14 +2,98 @@ package com.example.parkme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class slotes extends AppCompatActivity {
+public class slotes extends AppCompatActivity
+{
     TextView s1;
+    TextView s2;
+    TextView s3;
+    TextView s4;
+    TextView s5;
+    TextView s6;
+    TextView s7;
+    TextView s8;
+    TextView s9;
+    TextView s10;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slotes);
+        s1 =(TextView) findViewById(R.id.s1);
+        s2 =(TextView) findViewById(R.id.s2);
+        s3 =(TextView) findViewById(R.id.s3);
+        s4 =(TextView) findViewById(R.id.s4);
+        s5 =(TextView) findViewById(R.id.s5);
+        s6 =(TextView) findViewById(R.id.s6);
+        s7 =(TextView) findViewById(R.id.s7);
+        s8 =(TextView) findViewById(R.id.s8);
+        s9 =(TextView) findViewById(R.id.s9);
+        s10 =(TextView) findViewById(R.id.s10);
+        s1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                if (s1.getText().toString().isEmpty())
+                {
+                    s1.setText("EmPTY");
+                    s1.setTextColor(Color.GREEN);
+                }
+                else
+                    { s1.setText("FiLLED");
+                    s1.setTextColor(Color.RED);
+                    }
+            }
+            }
+        );
+        s2.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                if (s2.getText().toString().isEmpty())
+                {
+                    s2.setText("EmPTY");
+                    s2.setTextColor(Color.GREEN);
+                }
+                else
+                    { s2.setText("FiLLED");
+                    s2.setTextColor(Color.RED);
+                    }
+                }
+            }
+        );
+        s3.setOnClickListener(new Button.OnClickListener(){
+                                  public void onClick(View v)
+                                  {
+                                      if (s3.getText().toString().isEmpty())
+                                      {
+                                          s3.setText("EmPTY");
+                                          s3.setTextColor(Color.GREEN);
+                                      }
+                                      else
+                                      { s3.setText("FiLLED");
+                                          s3.setTextColor(Color.RED);
+                                      }
+                                  }
+                              }
+        );
+        s4.setOnClickListener(new Button.OnClickListener(){
+                                  public void onClick(View v)
+                                  {
+                                      if (s4.getText().toString().isEmpty())
+                                      {
+                                          s4.setText("EmPTY");
+                                          s4.setTextColor(Color.GREEN);
+                                      }
+                                      else
+                                      { s4.setText("FiLLED");
+                                          s4.setTextColor(Color.RED);
+                                      }
+                                  }
+                              }
+        );
     }
 }
